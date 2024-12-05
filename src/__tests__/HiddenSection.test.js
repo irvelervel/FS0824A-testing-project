88b66d22f5f8d04ce88a506be7e85fc5190a672b
Stefano Casasola <ste.casasola@gmail.com>
2024-12-05 12:17:29 +0100

    Starts creating tests

---
+++ src/__tests__/HiddenSection.test.js
@@ -0,0 +1,51 @@
+import {
+  getByAltText,
+  queryByAltText,
+  render,
+  screen,
+} from '@testing-library/react'
+import HiddenSection from '../components/HiddenSection'
+
+// to-do:
+// - il bottone all'avvio c'è
+// - il bottone all'avvio abbia label "MOSTRA"
+
+// vi suggerisco di raggruppare test simili in "suites"
+describe('Tutti i test del bottone', () => {
+  // ora andiamo a inserire i singoli test
+  test("il bottone all'avvio c'è", () => {
+    // 1) monto il componente nel Virtual Dom
+    render(<HiddenSection />)
+    // 2) ricerco l'elemento (in questo caso il bottone)
+    const button = screen.getByRole('button')
+    // screen.getByText('MOSTRA')
+    // 3) non intendiamo al momento interagirci, skip!
+    // 4) verifica ipotesi/tesi
+    expect(button).toBeInTheDocument()
+  })
+
+  test("il bottone all'avvio ha etichetta 'MOSTRA'", () => {
+    // 1) monto il componente nel Virtual Dom
+    render(<HiddenSection />)
+    // 2) ricerco l'elemento (in questo caso il bottone)
+    const button = screen.getByText(/mostra/i) // mOsTRa
+    // 3) skip
+    // 4) verifica ipotesi/tesi
+    expect(button).toBeInTheDocument()
+  })
+})
+
+// - il cane all'avvio non ci deve essere
+// - se clicco il bottone, il cane appare
+
+describe('tutti i test della card', () => {
+  test("all'avvio la card con il cane non c'è", () => {
+    // 1)
+    render(<HiddenSection />)
+    // 2)
+    const card = screen.queryByAltText('dog picture') // null
+    // 3)
+    // 4)
+    expect(card).toBeNull()
+  })
+})
